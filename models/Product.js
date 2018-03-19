386a58b4a0ec7240bbcec6abb01ac7937e27684b
luisrodriguez <codekommissar@gmail.com>
2018-03-19 12:32:35 -0400

    Set relationship between Product and User

--- models/Product.js
+++ models/Product.js
@@ -5,7 +5,8 @@ const productSchema = new Schema({
   name: String,
   image: String,
   url: String,
-  price: String
+  price: String,
+  _user: { type: Schema.Types.ObjectId, ref: 'User' }
 });
 
 mongoose.model("producs", productSchema);
