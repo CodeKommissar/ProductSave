f853a1798a6ba210de21a4c0e057e5ae32261248
luisrodriguez <codekommissar@gmail.com>
2018-03-29 10:33:05 -0400

    Create action creator to submit product

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -6,3 +6,7 @@ export const fetchUser = () => async (dispatch) => {
 
     dispatch({ type: FETCH_USER, payload: res.data });
 }
+
+export const submitProduct = values => {
+    return { type: "submit_product" };
+};
